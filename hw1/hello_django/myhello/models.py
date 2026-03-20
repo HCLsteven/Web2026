0fb408fb381a2e37d1664bf85ca98a96da096f56
HCLsteven <5862046@gmail.com>
2026-03-20 23:24:32 +0800

    Add files via upload

---
+++ hw1/hello_django/myhello/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    photo = models.CharField(max_length=200, blank=True)  # ← 改這行
+    location = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Course(models.Model):
+    department = models.CharField(max_length=100)
+    course_title = models.CharField(max_length=100)
+    instructor = models.CharField(max_length=100)
+    
---
+++ hw1/hello_django/myhello/views.py
@@ -0,0 +1,79 @@
+from rest_framework import status
+from rest_framework.response import Response
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from django.core.serializers.json import DjangoJSONEncoder
+import json
+import logging
+
+from .models import Post
+
+logger = logging.getLogger('django')
+
+
+@api_view(['GET'])
+def add_post(request):
+    title = request.GET.get('title', '')
+    content = request.GET.get('content', '')
+    photo = request.GET.get('photo', '')
+    location = request.GET.get('location', '')
+
+    new_post = Post()
+    new_post.title = title
+    new_post.content = content
+    new_post.photo = photo
+    new_post.location = location
+    new_post.save()
+
+    logger.debug("************* myhello_api:" + title)
+
+    if title:
+        return Response(
+            {"data": title + " insert!"},
+            status=status.HTTP_200_OK
+        )
+    else:
+        return Response(
+            {"res": "parameter: name is None"},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+@api_view(['GET'])
+def list_post(request):
+    posts = Post.objects.all().values()
+    return JsonResponse(list(posts), safe=False)
+    return Response(
+        {
+            "data": json.dumps(
+                list(posts),
+                sort_keys=True,
+                indent=1,
+                cls=DjangoJSONEncoder
+            )
+        },
+        status=status.HTTP_200_OK
+    )
+
+from .models import Course
+
+@api_view(['GET'])
+def add_course(request):
+    department = request.GET.get('department', '')
+    course_title = request.GET.get('course_title', '')
+    instructor = request.GET.get('instructor', '')
+
+    new_course = Course()
+    new_course.department = department
+    new_course.course_title = course_title
+    new_course.instructor = instructor
+    new_course.save()
+
+    return JsonResponse({
+        "res": "ok"
+    })
+
+@api_view(['GET'])
+def course_list(request):
+    courses = Course.objects.all().values()
+    return JsonResponse(list(courses), safe=False)
