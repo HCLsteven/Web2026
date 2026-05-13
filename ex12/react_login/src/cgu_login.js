b49dc5157a0cdc5f1671ca4f9fa17e266c3c2cd9
HCLsteven <5862046@gmail.com>
2026-05-13 14:51:01 +0800

    Update cgu_login.js

--- ex12/react_login/src/cgu_login.js
+++ ex12/react_login/src/cgu_login.js
@@ -34,7 +34,7 @@ const useStyles = makeStyles((theme) => ({
 
   image: {
     backgroundImage:
-      'url(https://images.unsplash.com/photo-1522202176988-66273c2fd55f)',
+      'url(https://images.unsplash.com/photo-1551796880-ddd03f861ae7?q=80&w=1128&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)',
 
     backgroundRepeat: 'no-repeat',
 
